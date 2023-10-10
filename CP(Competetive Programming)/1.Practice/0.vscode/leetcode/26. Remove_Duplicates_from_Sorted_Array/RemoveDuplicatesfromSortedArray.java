a8ebd55486013aa412872f8c8bea7550191edc67
Emon-Khan <rkemon94@gmail.com>
2023-10-10 18:44:11 +0600

    LeetCode Remove Duplicates Code

--- CP(Competetive Programming)/1.Practice/0.vscode/leetcode/26. Remove_Duplicates_from_Sorted_Array/RemoveDuplicatesfromSortedArray.java
+++ CP(Competetive Programming)/1.Practice/0.vscode/leetcode/26. Remove_Duplicates_from_Sorted_Array/RemoveDuplicatesfromSortedArray.java
@@ -1,86 +1,36 @@
 import java.util.*;
 
 class RemoveDuplicatesfromSortedArray {
-    /*
-     * public static void main(String[] args) {
-     * ArrayList nums = new ArrayList();
-     * Scanner scan = new Scanner(System.in);
-     * int n;
-     * n = scan.nextInt();
-     * int num;
-     * for (int i = 0; i < n; i++) {
-     * num = scan.nextInt();
-     * nums.add(num);
-     * }
-     * /*
-     * for (int i = 0; i < n; i++) {
-     * System.out.println(nums[i]);
-     * }
-     */
-    /*
-     * RemoveDuplicatesfromSortedArray obj = new RemoveDuplicatesfromSortedArray();
-     * int result = obj.removeDuplicates(nums);
-     * System.out.println(result);
-     * for (int i = 0; i < nums.size(); i++) {
-     * System.out.println(nums.get(i));
-     * }
-     * scan.close();
-     * }
-     * 
-     * public int removeDuplicates(ArrayList nums) {
-     * // Iterator<Integer> iterator = Arrays.stream(nums).iterator();
-     * for (int i = 0; i < nums.size(); i++) {
-     * for (int j = i + 1; j < nums.size(); j++) {
-     * if (nums.get(i) == nums.get(j)) {
-     * nums.remove(j);
-     * } else {
-     * break;
-     * }
-     * }
-     * }
-     * return nums.size();
-     * }
-     */
     public static void main(String[] args) {
-        int[] nums = new int[20];
         Scanner scan = new Scanner(System.in);
         int n;
         n = scan.nextInt();
+        int[] nums = new int[n];
         int num;
         for (int i = 0; i < n; i++) {
             num = scan.nextInt();
             nums[i] = num;
         }
-        /*
-         * for (int i = 0; i < n; i++) {
-         * System.out.println(nums[i]);
-         * }
-         */
+
         RemoveDuplicatesfromSortedArray obj = new RemoveDuplicatesfromSortedArray();
         int result = obj.removeDuplicates(nums);
-        System.out.println(result);
-        /*for (int i = 0; i < nums.length; i++) {
-            System.out.println(nums[i]);
-        }**/
+        System.out.print(result);
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+
         scan.close();
     }
 
     public int removeDuplicates(int[] nums) {
-        List list = Arrays.asList(nums);
-        for(List i: list)
-        // Iterator<Integer> iterator = Arrays.stream(nums).iterator();
-        for (int i = 0; i < list.size(); i++) {
-            for (int j = i + 1; j < list.size(); j++) {
-                if (list.get(i) == list.get(j)) {
-                    list.remove(j);
-                } else {
-                    break;
-                }
+        int j = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[j] = nums[i];
+                j++;
             }
         }
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i));
-        }
-        return list.size();
+        return j;
     }
+
 }
