ac226b481d13dc2c2035f1bbdcd2a083d916e8b2
Emon-Khan <rkemon94@gmail.com>
2024-04-29 04:11:33 +0600

    Solution of FirstNonRepeatingCharacter problem added

--- CP(Competetive Programming)/1.Practice/0. vscode/CodingNinja/FirstNonRepeatingCharacter/FirstNonRepeatingCharacter.py
+++ CP(Competetive Programming)/1.Practice/0. vscode/CodingNinja/FirstNonRepeatingCharacter/FirstNonRepeatingCharacter.py
@@ -1,16 +1,20 @@
 str = "AaaA"
 
+
 def firstNonRepeatingCharacter(str):
-    occurrence={}
+    occurrence = {}
     char = str[0]
     for item in str:
         if item in occurrence:
-            occurrence[item]+=1
+            occurrence[item] += 1
         else:
-            occurrence[item]=1
+            occurrence[item] = 1
+    # print(occurrence)
     for item in str:
-        if occurrence[item]==1:
-                char = item
-                break
+        if occurrence[item] == 1:
+            char = item
+            break
     return char
+
+
 print(firstNonRepeatingCharacter(str))
