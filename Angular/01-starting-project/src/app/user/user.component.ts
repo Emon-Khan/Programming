97b57044e4d58abc0cea41ab9613a62c39b325af
Emon-Khan <rkemon94@gmail.com>
2024-08-08 15:32:30 +0600

    Till 49 no video has been completed

---
+++ Angular/01-starting-project/src/app/user/user.component.ts
@@ -0,0 +1,36 @@
+import {
+  Component,
+  computed,
+  EventEmitter,
+  input,
+  Input,
+  Output,
+  output,
+} from '@angular/core';
+import { User } from './user.model';
+// type User = {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// };
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
+  @Output() select = new EventEmitter<string>();
+  // select = output<string>();
+
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
+}
